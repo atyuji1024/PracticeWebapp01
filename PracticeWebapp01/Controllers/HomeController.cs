21ca650d69e7f356cdbde0c6d0b5be694c2e7dbf
青木 雄司 <at_yuji1024@outlook.jp>
2020-09-10 22:51:44 +0900

    feat: 画面追加

--- PracticeWebapp01/Controllers/HomeController.cs
+++ PracticeWebapp01/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace PracticeWebapp01.Controllers
         {
             return View();
         }
+        public IActionResult MyBase()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
